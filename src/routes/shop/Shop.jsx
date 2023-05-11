a8dcdade4c9a3afb4ec4c2cbb320a42ecbe07498
Kimyeonho <kyh0703@nate.com>
2023-05-11 23:50:30 +0900

    [Feature] Category Preview

--- src/routes/shop/Shop.jsx
+++ src/routes/shop/Shop.jsx
@@ -1,17 +1,23 @@
-import { useContext } from 'react';
+import React, { Fragment, useContext } from 'react';
 
-import { ProductsContext } from '../../contexts/ProductsContext';
+import { CategoriesContext } from '../../contexts/CategoriesContext';
 import ProductCard from '../../components/product/ProductCard';
+import CategoryPreview from '../../components/category/CategoryPreview';
+
 import './Shop.scss';
 
 function Shop(props) {
-  const { products } = useContext(ProductsContext);
+  const { categoriesMap } = useContext(CategoriesContext);
+  console.log(categoriesMap);
 
   return (
-    <div className='products-container'>
-      {products.map((product) => (
-        <ProductCard key={product.id} product={product} />
-      ))}
+    <div className='shop-container'>
+      {Object.keys(categoriesMap).map((title) => {
+        const products = categoriesMap[title];
+        return (
+          <CategoryPreview key={title} title={title} products={products} />
+        );
+      })}
     </div>
   );
 }
