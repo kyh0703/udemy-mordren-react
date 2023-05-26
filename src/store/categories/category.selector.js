d23ffd6634bf2d1481e6ad94e01b4d6900004790
Kimyeonho <kyh0703@nate.com>
2023-05-26 23:22:57 +0900

    [Feature] Reselect Library

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,8 +1,19 @@
-export const selectCategoriesMap = (state) => {
-  console.log('selector fired');
-  return state.categories.categories.reduce((acc, category) => {
-    const { title, items } = category;
-    acc[title.toLowerCase()] = items;
-    return acc;
-  }, {});
-};
+import { createSelector } from 'reselect';
+
+// 입력값이 바뀌지 않는 한 출력값이 바뀌지 않는다.
+const selectCategoryReducer = (state) => state.categories;
+
+export const selectCategories = createSelector(
+  [selectCategoryReducer],
+  (categoriesSlice) => categoriesSlice.categories
+);
+
+export const selectCategoriesMap = createSelect(
+  [selectCategories],
+  (categories) =>
+    categories.reduce((acc, category) => {
+      const { title, items } = category;
+      acc[title.toLowerCase()] = items;
+      return acc;
+    }, {})
+);
