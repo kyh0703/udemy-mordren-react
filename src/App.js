e6af3944be322c2df8f5879b60f754a68c830273
Kimyeonho <kyh0703@nate.com>
2023-06-07 17:09:52 +0900

    [Feture] Init

--- src/App.js
+++ src/App.js
@@ -1,31 +1,22 @@
-import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
 import { Routes, Route } from 'react-router-dom';
 
 import Home from './routes/home/home.component';
-import Shop from './routes/shop/shop.component';
 import Navigation from './routes/navigation/navigation.component';
-import SignIn from './routes/authetication/authentication.component';
+import Authentication from './routes/authentication/authentication.component';
+import Shop from './routes/shop/shop.component';
 import Checkout from './routes/checkout/checkout.component';
-import { checkUserSession } from './store/user/user.action';
 
-function App() {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(checkUserSession());
-  }, []);
-
+const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path='shop/*' element={<Shop />} />
-        <Route path='auth' element={<SignIn />} />
+        <Route path='auth' element={<Authentication />} />
         <Route path='checkout' element={<Checkout />} />
       </Route>
     </Routes>
   );
-}
+};
 
 export default App;
--- src/routes/home/home.component.tsx
+++
@@ -1,7 +0,0 @@
-import Directory from '../../components/directory/directory.component';
-
-function Home() {
-  return <Directory />;
-}
-
-export default Home;
